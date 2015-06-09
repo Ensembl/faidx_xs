dae5c4001e5eb9a33eed1d90c933dcbe6fd6e0fd
Rishi Nag <rn6@sanger.ac.uk>
2015-06-09 16:17:21 +0100

    convert gzip script takes a directory argument

--- scripts/convert_gz_2_bgz.sh
+++ scripts/convert_gz_2_bgz.sh
@@ -1,4 +1,19 @@
+#!/bin/bash
 
+script_name="convert_gz_2_bgz.sh"
+
+if [ -z "$1" ]
+  then
+    echo "Usage: $script_name [directory with gz files]"
+    exit 1
+fi
+
+if [ ! -d "$1" ]; then
+    echo "$script_name: directory $1 not found"
+    exit 1
+fi
+
+cd $1
 
 find . -name "*.gz" -print0 | while read -d $'\0' file
 do
@@ -7,5 +22,9 @@ do
    gzip -d $file
    bgzip $filename
 done
+
+cd -
+
+exit $?
 
 

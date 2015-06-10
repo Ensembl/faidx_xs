6235089b9e20b24a23240e7a397b7077b873e8bd
Rishi Nag <rn6@sanger.ac.uk>
2015-06-10 09:41:00 +0100

    can now specify file arguments

--- scripts/convert_gz_2_bgz.sh
+++ scripts/convert_gz_2_bgz.sh
@@ -2,28 +2,34 @@
 
 script_name="convert_gz_2_bgz.sh"
 
-if [ -z "$1" ]
-  then
-    echo "Usage: $script_name [directory with gz files]"
+if [ -z "$1" ]; then
+    echo "Usage: $script_name [gzipped file|directory with gz files]"
     exit 1
 fi
 
-if [ ! -d "$1" ]; then
-    echo "$script_name: directory $1 not found"
+
+if [ -f "$1" ]; then
+    dir="${1%/*}"
+    file="${1##*/}"
+    filename="${1%.*}"
+    echo "Making bgzip file $filename from $file"
+    cd $dir
+    gzip -d $file
+    bgzip $filename
+elif [ -d "$1" ]; then
+    cd $1
+    find . -name "*.gz" -print0 | while read -d $'\0' file
+    do
+        echo $file
+        filename="${file%.*}"
+        gzip -d $file
+        bgzip $filename
+    done
+    cd -
+else
+    echo "$script_name: argument $1 not found"
     exit 1
 fi
-
-cd $1
-
-find . -name "*.gz" -print0 | while read -d $'\0' file
-do
-   echo $file
-   filename="${file%.*}"
-   gzip -d $file
-   bgzip $filename
-done
-
-cd -
 
 exit $?
 

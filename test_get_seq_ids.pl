bb5601823ff108a9c7f6c2abb697224b3412981a
Rishi Nag <rishi@ebi.ac.uk>
2015-10-28 09:34:06 +0000

    test updated for additional file

--- test_get_seq_ids.pl
+++ test_get_seq_ids.pl
@@ -6,7 +6,10 @@ use warnings ;
 Faidx::print_hello() ;
 
 my $fasta = "t/Saccharomyces_cerevisiae.R64-1-1.dna.chromosome.I.fa.gz" ;
-my $location = "I:1-100" ;
 my $index = Faidx->new($fasta);
+$index->get_all_sequence_ids();
 
+
+$fasta = "/home/rishi/data/faidx/Xiphophorus_maculatus.Xipmac4.4.2.dna_rm.toplevel.fa.gz" ;
+$index = Faidx->new($fasta);
 $index->get_all_sequence_ids();
